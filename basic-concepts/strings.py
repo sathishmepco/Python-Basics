180147c16a9b1629d7d604889b0b3bdc9ac9a8fd
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-04 16:08:16 +0530

    Python Basic Concepts - String

--- basic-concepts/strings.py
+++ basic-concepts/strings.py
@@ -1,7 +1,21 @@
 str = 'Hello Facebook'
 print( str )
-print( str[0] )
-print( str[2:5] )
-print( str[2:] )
-print( str * 2)
-print( str + ' App')
+print('Character at 0 index ::',str[0] )
+print('Character at 10 index ::',str[10] )
+print('Last character ::',str[-1])
+print('Second Last character ::',str[-2])
+print('Characters from index 2 to 5 ::', str[2:5] )
+print('Characters from index 2 to end of the string ::', str[2:] )
+print('Characters from beginning to positino to 5 ::',str[:5])
+print('Characters from the second last to end of the stirng ::',str[-2:])
+print('Length of the string is ::',len(str))
+print("""
+Displaying 
+multiple
+lines
+""")
+print('Py''thon')
+print("5 times displaying name :: ",5*'Sathish ')
+print('This will join '
+		'two lines together')
+print('Concatenate two strings ::',str+' Developer')		
