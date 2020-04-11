202133fd7ceeefdfa900571aae29ba7cd6150f93
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-11 15:36:12 +0530

    Queue

--- basic-concepts/data_structures/queue_demo.py
+++ basic-concepts/data_structures/queue_demo.py
@@ -9,3 +9,15 @@ print('Pop the item from the queue :',queue.popleft())
 print('After Pop :',queue)
 print('Pop the item from the queue :',queue.popleft())
 print('After Pop :',queue)
+
+'''
+OUTPUT
+------
+The items in the queue are : deque(['Eric', 'John', 'Michael'])
+After appending Terry : deque(['Eric', 'John', 'Michael', 'Terry'])
+After appending Graham : deque(['Eric', 'John', 'Michael', 'Terry', 'Graham'])
+Pop the item from the queue : Eric
+After Pop : deque(['John', 'Michael', 'Terry', 'Graham'])
+Pop the item from the queue : John
+After Pop : deque(['Michael', 'Terry', 'Graham'])
+'''
