fd229101ee0df04a2d9f63a588f04aa21b3b6881
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-11 12:36:32 +0530

    Python basic - Stack

---
+++ basic-concepts/stack.py
@@ -0,0 +1,12 @@
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+print('The Stack is :',stack)
+print('Pop the element :',stack.pop())
+print('After Pop the stack is :',stack)
+print('Pop the element :',stack.pop())
+print('Pop the element :',stack.pop())
+print('After Pop the stack is :',stack)
+stack.append(10)
+print('Push the element 10')
+print('After Push the stack is :',stack)
