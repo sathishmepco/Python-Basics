f90c441fc982dbc0eb887233341d26b514f2da9f
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-15 20:29:02 +0530

    string

--- basic-concepts/strings.py
+++ basic-concepts/strings.py
@@ -25,3 +25,41 @@ str = '      hello'
 print('String left trim trim (lstrip) function :',str.lstrip())
 str = 'hello     '
 print('String right trim (rstrip) function :',str.rstrip())
+str = str.upper()
+print('Upper case conversion :',str)
+str = str.lower()
+print('Lower case conversion :',str)
+str = str.replace('l','L')
+print('Replace l with L :',str)
+str = 'Hello World'
+print('Splitting Hello World :',str.split(' '))
+
+
+'''
+Hello Facebook
+Character at 0 index :: H
+Character at 10 index :: b
+Last character :: k
+Second Last character :: o
+Characters from index 2 to 5 :: llo
+Characters from index 2 to end of the string :: llo Facebook
+Characters from beginning to positino to 5 :: Hello
+Characters from the second last to end of the stirng :: ok
+Length of the string is :: 14
+
+Displaying
+multiple
+lines
+
+Python
+5 times displaying name ::  Sathish Sathish Sathish Sathish Sathish
+This will join two lines together
+Concatenate two strings :: Hello Facebook Developer
+String trim (strip) function : hello
+String left trim trim (lstrip) function : hello
+String right trim (rstrip) function : hello
+Upper case conversion : HELLO
+Lower case conversion : hello
+Replace l with L : heLLo
+Splitting Hello World : ['Hello', 'World']
+'''
