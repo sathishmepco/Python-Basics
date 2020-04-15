f5c65da8cdd2dcb2d9f8aafea9b849037f21a4f3
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-15 20:16:02 +0530

    casting

---
+++ basic-concepts/casting.py
@@ -0,0 +1,41 @@
+print('Integer casting')
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+print('Float casting')
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+print('String casting')
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+'''
+Integer casting
+1
+2
+3
+Float casting
+1.0
+2.8
+3.0
+4.2
+String casting
+s1
+2
+3.0
+'''
