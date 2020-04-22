055b025bd21a0e4749b815d648306ca1e007fb44
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-22 20:32:16 +0530

    functions

--- basic-concepts/modules/main.py
+++ basic-concepts/modules/main.py
@@ -3,6 +3,9 @@ import platform
 import calculator as calc
 from employee import person
 
+def function(country="India"):
+	print("I am from "+country)
+
 a = 100
 b = 101
 
@@ -13,3 +16,17 @@ print('Person name -',person['name'])
 print('Person country -',person['country'])
 print('Operating System is -',platform.system())
 print('Hello, ' + os.getlogin() + '! How are you?')
+function("America")
+function()
+
+'''
+Sum of 100 and 101 is - 201
+Is 100 even - True
+Is 101 even - False
+Person name - Sathish
+Person country - India
+Operating System is - Windows
+Hello, Seng! How are you?
+I am from America
+I am from India
+'''
