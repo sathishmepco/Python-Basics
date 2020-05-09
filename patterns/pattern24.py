14ef93749d375da56897b6fa06919096d44be0dc
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-09 11:37:34 +0530

    Pattern 24 - TechGig

---
+++ patterns/pattern24.py
@@ -0,0 +1,24 @@
+def main():
+    n = int(input().strip())
+    CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(n):
+        for j in range(n-i-1):
+            print(end='  ')
+        for j in range(i+1):
+            if j == 0:
+                print(CHARS[i],end='')
+            else:
+                print(' '+CHARS[i],end='')
+        if i < n-1:
+            print()
+main()
+'''
+Input
+5
+Output
+        A
+      B B
+    C C C
+  D D D D
+E E E E E
+'''
