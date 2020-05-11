1a6caf0ab2f75cbe3cd41367bc1f1cd4b0257b8c
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-11 20:08:57 +0530

    Pattern 27 - TechGig

---
+++ patterns/pattern27.py
@@ -0,0 +1,23 @@
+def main():
+    n = int(input().strip())
+    for i in range(n):
+        for j in range(n-i-1):
+            print(end='  ')
+        for j in range(i+1):
+            if j == 0:
+                print(end='*')
+            else:
+                print(end=' *')
+        if i < n-1:
+            print()
+main()
+'''
+Input
+5
+Output
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+'''
