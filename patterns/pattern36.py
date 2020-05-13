5c2fca674c9a9011931e9e9dc3dab018d2054440
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-13 22:47:13 +0530

    Patter 36 - TechGig

---
+++ patterns/pattern36.py
@@ -0,0 +1,22 @@
+def main():
+    n = int(input().strip())
+    CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(n):
+        for j in range(i+1):
+            if j == 0:
+                print(CHARS[j],end='')
+            else:
+                print(' '+CHARS[j],end='')
+        if i < n-1:
+            print()
+main()
+'''
+Input
+5
+Output
+A
+A B
+A B C
+A B C D
+A B C D E
+'''
