a8e9e0d5d137046b99f8063b3a68e383a28fe1f9
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-06-17 23:58:20 +0530

    Most Common - Collection

--- basic-concepts/collections/counter_demo.py
+++ basic-concepts/collections/counter_demo.py
@@ -7,5 +7,29 @@ def main():
     print(c)
     print('Most Common :: ')
     print(c.most_common(1))
+	
+    c = Counter('malayalam')
+    print('From String :: malayalam')
+    print(c.most_common())
+    print('a occured :: ',c['a'])
+    print('z occured :: ',c['z'])
+	
+    print('Counter to List (reverse)')
+    l = list(c.elements())
+    print('List is :: ',l)
+	
+    print('Subtract of two Counters :: ')
+    c1 = Counter(a=4, b=2, c=0, d=-2)
+    c2 = Counter(a=1, b=2, c=3, d=4)
+    c1.subtract(c2)
+    print(c1)
 		
 main()
+
+'''
+Counter({'blue': 3, 'red': 2, 'green': 1})
+Most Common ::
+[('blue', 3)]
+From String :: malayalam
+[('a', 4), ('m', 2), ('l', 2), ('y', 1)]
+'''
